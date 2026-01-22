d086de3e9f289f90793850e596834de32169497e
Sarfaraj Rahman <sarfarajrahmn@gmail.com>
2026-01-23 01:39:43 +0600

    update

---
+++ app/abc/hello/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Hello from the hello page</div>;
+};
+
+export default page;
---
+++ app/abc/layout.tsx
@@ -0,0 +1,8 @@
+export default function AbcLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>hello from the abc layout</h1>
+      {children}
+    </div>
+  );
+}
