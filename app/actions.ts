2f30795ba19e4c228611cf721b2e6d8866185253
Sarfaraj Rahman <sarfarajrahmn@gmail.com>
2026-02-24 16:43:27 +0600

    update

--- app/actions.ts
+++ app/actions.ts
@@ -6,7 +6,7 @@ import { fetchMutation } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
 import { redirect } from "next/navigation";
 import { getToken } from "@/lib/auth-server";
-import { revalidatePath, updateTag } from "next/cache";
+import { updateTag } from "next/cache";
 
 export async function createBlogAction(values: z.infer<typeof postSchema>) {
   try {
@@ -53,6 +53,6 @@ export async function createBlogAction(values: z.infer<typeof postSchema>) {
     };
   }
 
-  revalidatePath("blog");
+  updateTag("blog");
   return redirect("/blog");
 }
